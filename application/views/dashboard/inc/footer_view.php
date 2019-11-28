725aca03bbb205ec164af76538f1426a34676fe1
SVETO <svetostoj@hotmail.com>
2019-11-28 12:31:48 +1100

    first commit

---
+++ application/views/dashboard/inc/footer_view.php
@@ -0,0 +1,7 @@
+</div>
+   <!--end wrapper-->
+   <footer>
+    &copy; <?=date('Y')?>
+</footer>
+    </body>
+</html>
---
+++ application/views/dashboard/inc/header_view.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html land="en">
+    <head>
+        <title> dev-CI  </title>
+        <link rel="stylesheet" href="http://localhost:8888/dev-ci/public/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?= base_url()?>public/css/style.css" />        
+
+        <script src="<?= base_url()?>public/js/jquery.js"></script>
+        <script src="<?= base_url()?>public/js/bootstrap.js"></script>
+        
+    </head>
+    <body>
+        
+        <header>...  </header>
+        <nav class="navbar">
+            <div class="navbar-inner">
+                <span class="brand">codeIgniter</span>
+            <ul class="nav">
+                <li><a href="#">Dashboard</a> </li>
+
+            <li><a href="#">User</a></li>
+
+            <li><a href="<?=site_url("dashboard/logout")?>">Logout</a></li>
+            </ul>
+            </div>
+        </nav>
+       <!--start wrapper--> 
+
+        <div class="wrapper">
+            
+     
